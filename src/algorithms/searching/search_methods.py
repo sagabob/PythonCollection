f6afe6b054e6de134d78af70a91a3717b695a59f
DESKTOP-768I4JG\bobpham <bobpham.tdp@gmail.com>
2022-08-13 02:56:17 +1000

    set up and add the first algorithms

---
+++ src/algorithms/searching/search_methods.py
@@ -0,0 +1,12 @@
+def binary_search(arr, start, end, key):
+
+    while start <= end:
+        mid = int(start + (end - start)/2)
+        
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
---
+++ src/algorithms/searching/test.py
@@ -0,0 +1,7 @@
+
+import search_methods
+
+arr = [4, 6, 9, 13, 14, 18, 21, 24, 38]
+x = 13
+result = search_methods.binary_search(arr, 0, len(arr)-1, x)
+print(result)
